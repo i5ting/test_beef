0f4a52a57bf964573511d4e1d65eaf28b967e9f7
i5ting <i5ting@126.com>
2013-05-07 18:36:15 +0800

    first

---
+++ init.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+#
+#  init.sh
+#  create ios project dir like rails dir
+#
+#  Created by sang alfred on 3/6/13.
+#  Copyright (c) 2013 no320.com. All rights reserved.
+# 
+
+# ➜  test git:(master) ✗ ls
+# app    config db     doc    log    script test   vendor
+# ➜  test git:(master) ✗ ls -R
+# app    config db     doc    log    script test   vendor
+# 
+# ./app:
+# assets      controllers helpers     models      views
+# 
+# ./app/assets:
+# images
+# 
+# ./app/assets/images:
+# 
+# ./app/controllers:
+# 
+# ./app/helpers:
+# 
+# ./app/models:
+# 
+# ./app/views:
+# 
+# ./config:
+# environments initializers locales
+# 
+# ./config/environments:
+# 
+# ./config/initializers:
+# 
+# ./config/locales:
+# 
+# ./db:
+# 
+# ./doc:
+# 
+# ./log:
+# 
+# ./script:
+# 
+# ./test:
+# 
+# ./vendor:
+#
+#
+#
+#
+
+mkdir -p src/app/assets/images
+mkdir -p src/app/assets/xibs  #new
+mkdir -p src/app/assets/bundles #new
+mkdir -p src/app/controllers 
+mkdir -p src/app/helpers        
+mkdir -p src/app/models      
+mkdir -p src/app/views
+mkdir -p src/app/apis
+
+mkdir src/db
+mkdir src/doc
+mkdir src/log
+mkdir src/test
+mkdir src/script
+mkdir src/server
+
+mkdir -p src/config/environments
+mkdir -p src/config/initializers
+mkdir -p src/config/locales
+
+sqlite3 src/db/sina_finance_ios.db ""
+
+mkdir vendor
+
+touch README.md
+
+mkdir test
---
+++ test/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  sinafinance
+//
+//  Created by sang on 5/7/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
